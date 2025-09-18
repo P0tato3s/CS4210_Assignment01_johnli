24a9e88b722eebfb4b18f7a9696e270f997ba50e
John <lijohn2004@gmail.com>
2025-09-18 14:55:49 -0700

    changed heading/notes to correct information

--- decision_tree.py
+++ decision_tree.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: John Li
+# FILENAME: decision_tree.py
+# SPECIFICATION: Creates and visualizes a decision tree for contact lens recommendation
 # FOR: CS 4210- Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 5 hour
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
